c0b08fbc1b0a2799055958cff99c0a5ef7e916e5
Tyler887 <tylermageeshields@gmail.com>
2022-04-03 22:53:20 +0100

    Update repl.py

--- repl.py
+++ repl.py
@@ -25,10 +25,20 @@ try:
             code.write(f"import {i} # Import {i}\n")
         with open(f"{user_home}/.pythonrepl_code", "a") as code:
           code.write("\n" + repl)
+        dead_end = False
+        if repl.endswith(":"):
+          while not dead_end:
+            addtorepl = input("         ")
+            if addtorepl != "":
+              repl = repl + "\n" + addtorepl
+              addtorepl = ""
+            else:
+              dead_end = True
         os.system(f"python{sys.version_info.major} {user_home}/.pythonrepl_code")
         os.unlink(f"{user_home}/.pythonrepl_code")
         with open(f"{user_home}/.pythonrepl_code", "w") as code:
               code.write("")
         repl = ""
+        dead_end = False
 except KeyboardInterrupt:
   print("\nKeyboardInterrupt")
